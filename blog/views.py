28aa2e3367d27ce0122a501cb301641b19baf9b0
karlos <carlosadriano_gouveia@hotmail.com>
2017-09-25 10:18:31 -0300

    Adicao bootstrap.

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+    posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('-data_publicacao')
     return render(request, 'blog/post_list.html', {'posts': posts})
